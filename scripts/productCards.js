4976ad83236924faa8ec6d4df3ace3e3886832fe
sebastianbanagasta <sebastian.09.10.1995@gmail.com>
2024-04-25 10:37:29 -0500

    se optimizo las vistas al HTML

--- scripts/productCards.js
+++ scripts/productCards.js
@@ -1,47 +1,125 @@
 
+const products = [
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+    {
+        id : "A100",
+        linkTo : "./details.html",
+        images : "/assets/mock1.jpg",
+        title : "iPad Pro 12.9",
+        color : "Silver",
+        price : 750.000,
+        discount : "50% Off",
+    },
+]
 
-const productosSelector = document.getElementById('products')
+const printCards = (arrayOfProducts,idSelector) => {
+    const productosSelector = document.getElementById(idSelector)
+    let productsTemplate = ""
 
-
-const createCard = (product)=>{
-    return `
-    <article class="product-card">
-        <a class="product-card" href="./details.html">
-            <img src="${product.images[0]}" alt="${product.id}" class="product-img">
-            <div class="product-info">
-                <span class="product-title">${product.title}</span>
-                <span class="product-description">Silver</span>
-                <div class="product-price-block">
-                    <span class="price">$750.000</span>
-                    <span class="discount">50% Off</span>
+    const createCard = (product)=>{
+        return `
+        <article class="product-card">
+            <a class="product-card" href="${product.linkTo}">
+                <img src="${product.images}" alt="${product.id}" class="product-img">
+                <div class="product-info">
+                    <span class="product-title">${product.title}</span>
+                    <span class="product-description">${product.color}</span>
+                    <div class="product-price-block">
+                        <span class="price">$${product.price}</span>
+                        <span class="discount">${product.discount}</span>
+                    </div>
+                    <div class="product-tax-policy">Incluye impuesto País ...</div>
                 </div>
-                <div class="product-tax-policy">Incluye impuesto País ...</div>
-            </div>
-        </a>
-    </article>
-    `
-}
-let productsTemplate = ``
-for(const element of products){
-    productsTemplate = productsTemplate + createCard(element)
+            </a>
+        </article>
+        `
+    }
+
+    for(const element of arrayOfProducts){
+        productsTemplate = productsTemplate + createCard(element)
+    }
+
+    productosSelector.innerHTML = productsTemplate
 }
 
-productosSelector.innerHTML = productsTemplate
-
-/*
-<article class="product-card">
-    <a class="product-card" href="./details.html">
-        <img src="/assets/mock1.jpg" alt="producto" class="product-img">
-        <div class="product-info">
-            <span class="product-title">iPad Pro 12.9</span>
-            <span class="product-description">Silver</span>
-            <div class="product-price-block">
-                <span class="price">$750.000</span>
-                <span class="discount">50% Off</span>
-            </div>
-            <div class="product-tax-policy">Incluye impuesto País ...</div>
-        </div>
-    </a>
-</article>
-*/
-productosSelector.innerHTML = productsTemplate
+printCards(products,"products")
